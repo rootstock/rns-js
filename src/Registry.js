2e86ec270437166d2aefbbc077aad2da544297e5
Ilan Olkies <ilanolkies@outlook.com>
2019-03-13 20:02:18 -0300

    Document Registry methods

--- src/Registry.js
+++ src/Registry.js
@@ -3,40 +3,84 @@ import { registryAddress } from './contracts';
 import { hash as namehash } from 'eth-ens-namehash';
 
 export default class Registry {
-  constructor (web3, options = {}) {
-    this.contract = web3.eth.Contract(registryAbi, registryAddress, options);
+  /**
+   * @param {Web3} web3 web3.js lib
+   *
+   * @constructor
+   */
+  constructor (web3) {
+    this.contract = web3.eth.Contract(registryAbi, registryAddress);
   }
 
+  /**
+   * Gets a name's owner
+   * @param {String} name
+   *
+   * @returns {Promise<Address>}
+   */
   owner (name) {
     const hash = namehash(name);
 
     return this.contract.methods.owner(hash).call()
   }
 
+  /**
+   * Sets a name's owner
+   * @param {String} name
+   * @param {Address} owner
+   *
+   * @returns {Promise<TxHash>}
+   */
   setOwner (name, owner) {
     const hash = namehash(name);
 
     return this.contract.methods.setOwner(hash, owner).send();
   }
 
+  /**
+   * Gets a name's resolver
+   * @param {String} name
+   *
+   * @returns {Promise<Address>}
+   */
   resolver (name) {
     const hash = namehash(name);
 
     return this.contract.methods.resolver(hash).call()
   }
 
+  /**
+   * Sets a name's resolver
+   * @param {String} name
+   * @param {Address} resolver
+   *
+   * @returns {Promise<TxHash>}
+   */
   setResolver (name, resolver) {
     const hash = namehash(name);
 
     return this.contract.methods.setResolver(hash, resolver).send();
   }
 
+  /**
+   * Gets a name's time to live
+   * @param {String} name
+   *
+   * @returns {Promise<Address>}
+   */
   ttl (name) {
     const hash = namehash(name);
 
     return this.contract.methods.ttl(hash).call()
   }
 
+  /**
+   * Sets a name's time to live
+   * @param {String} name
+   * @param {Number} ttl
+   *
+   * @returns {Promise<TxHash>}
+   */
   setTtl (name, ttl) {
     const hash = namehash(name);
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import Web3 from 'web3';
 import Registry from './Registry';
 
 export default class RNS {
+  /**
+   * @param {AbstractSocketProvider|HttpProvider|CustomProvider|String} web3Provider web3.js provider
+   * @param {Object} options web3.js options
+   *
+   * @constructor
+   */
   constructor (web3Provider, options = {}) {
     const web3 = new Web3(web3Provider, options);
 
-    this.registry = new Registry(web3, options);
+    this.registry = new Registry(web3);
   }
 }
